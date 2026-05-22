b69b51a686284f224191fd8e48f74b24bf450128
Muammer Kizilaslan <Muammer.k@gmx.de>
2026-05-22 07:04:03 +0200

    Sync Mobile-Pages aus Haupt-Repo 2026-05-22

--- sw.js
+++ sw.js
@@ -11,15 +11,15 @@
    dann die neue (siehe Sprint-148-Befund "andere Sicht zuerst").
 */
 
-const CACHE = 'karriaro-mobile-v154';
+const CACHE = 'karriaro-mobile-v155';
 const OFFLINE_URL = '/offline.html';
 const SHELL = [
     '/',
     '/offline.html',
     '/css/mobile.css',
-    '/css/mobile-overrides.css?v=151',
+    '/css/mobile-overrides.css?v=152',
     '/css/tokens.css',
-    '/js/m-interactions.js?v=151',
+    '/js/m-interactions.js?v=152',
     '/icons/icon-192.png',
     '/icons/icon-512.png',
     '/icons/apple-touch-icon.png',
